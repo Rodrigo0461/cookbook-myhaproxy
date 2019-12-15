beaf4330b38b51ced51d99adb351197c36c55b26
Rodrigo Pichinual <rodrigo.pichinual@gmail.com>
2019-12-15 03:30:02 -0300

    Added cookbook haproxy

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,21 @@
 # Cookbook:: myhaproxy
 # Recipe:: default
 #
-# Copyright:: 2019, The Authors, All Rights Reserved.
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+haproxy_install 'package'
+
+haproxy_frontend 'http-in' do
+  bind '*:80'
+  default_backend 'servers'
+end
+
+haproxy_backend 'servers' do
+  server [
+  	'web1 192.168.10.43:80 maxconn 32'
+end
+
+
+haproxy_service 'haproxy' do
+	subscribes :reload, 'template[/etc/haproxy/haproxy.cfg]', :immediately
+end
